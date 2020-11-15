30013288f9bc6dfccd827b42d1b196ae495d45f9
Lukas <lanson@ucsd.edu>
2020-11-15 15:44:03 -0600

    finished contact section

--- js/index.js
+++ js/index.js
@@ -13,3 +13,12 @@ $(document).on("scroll", function () {
       }
     }
 })
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+  alert("coppied");
+}
